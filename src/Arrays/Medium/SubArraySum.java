c0bcfa01103bea5a207c2f5cb0519b782c0fa6ac
belvinleo <89306672+belvinleo@users.noreply.github.com>
2025-10-16 17:00:12 +0530

    arrays done

---
+++ src/Arrays/Medium/SubArraySum.java
@@ -0,0 +1,16 @@
+package Arrays.Hard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraySum {
+    public static int subarraySum(int[] nums, int k){
+        Map<Integer, Integer> freq = new HashMap<>();
+        freq.put(0, 1);
+
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
